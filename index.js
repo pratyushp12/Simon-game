7c6bb00c562f5c1f73d36903e1208453cba8f68f
pratyushp12 <51055538+pratyushp12@users.noreply.github.com>
2021-01-10 18:25:41 +0530

    Initial commit after local devlopment

---
+++ index.js
@@ -0,0 +1,84 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+var level=0;
+
+var started = true;
+
+$(document).on("keydown",function(){
+  if(started){
+    nextSequence();
+    started = false;
+  }
+});
+
+$(".btn").on("click",function(){
+var userChosenColour = $(this).attr("id");
+userClickedPattern.push(userChosenColour);
+playSound(userChosenColour);
+animatePress(userChosenColour);
+checkAnswer(userClickedPattern.length-1);
+});
+
+function nextSequence(){
+
+  userClickedPattern = [];
+  $("#level-title").html("Level "+level);
+  var randomNumber =Math.floor(Math.random()*4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+  level++;
+}
+
+function playSound(name){
+
+  var audio= new Audio("sounds/"+name+".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $("#"+currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#"+currentColour).removeClass("pressed");
+  },100);
+}
+
+function checkAnswer(currentLevel){
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+    console.log("success");
+
+    //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+    if (userClickedPattern.length === gamePattern.length){
+
+      //5. Call nextSequence() after a 1000 millisecond delay.
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+
+    }
+
+  } else {
+
+    var audio= new Audio("sounds/wrong.mp3");
+    audio.play();
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over")
+    },200);
+    $("#level-title").html("Game Over! Press any Key to Restart");
+    startOver();
+  }
+}
+
+
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  started = true;
+}
