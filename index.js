ed667b2b7fa2e21a8db667ae464b4344e2bbf6db
pratyushp12 <51055538+pratyushp12@users.noreply.github.com>
2021-03-24 12:28:51 +0530

    refactored code

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+//store the colours in an Array
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+//Create Empty Array to store sequence of colours
 var gamePattern = [];
 
+//Array to store the sequence of buttons pressed by User
 var userClickedPattern = [];
 
+//start with level=0
 var level=0;
 
+//Initially game started is set to true
 var started = true;
 
+//checks if game is about to start or is already started
 $(document).on("keydown",function(){
   if(started){
     nextSequence();
@@ -15,6 +21,7 @@ $(document).on("keydown",function(){
   }
 });
 
+//adds the colour of button that user clicked to the Arraya and then checks it with the correct pattern
 $(".btn").on("click",function(){
 var userChosenColour = $(this).attr("id");
 userClickedPattern.push(userChosenColour);
@@ -23,6 +30,7 @@ animatePress(userChosenColour);
 checkAnswer(userClickedPattern.length-1);
 });
 
+//adds the next sequence to gamePattern Array and increments the level
 function nextSequence(){
 
   userClickedPattern = [];
@@ -35,12 +43,15 @@ function nextSequence(){
   level++;
 }
 
+
+//play the sound when button is clicked
 function playSound(name){
 
   var audio= new Audio("sounds/"+name+".mp3");
   audio.play();
 }
 
+//Animate(Fade in-out) the button when pressed
 function animatePress(currentColour){
   $("#"+currentColour).addClass("pressed");
   setTimeout(function(){
@@ -48,22 +59,26 @@ function animatePress(currentColour){
   },100);
 }
 
+//checks if the user is pressing the correct sequence or not
 function checkAnswer(currentLevel){
+  //correct sequence
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
     console.log("success");
 
-    //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+    //If the user got the most recent answer correct, then check that they have finished their sequence with another if statement.
     if (userClickedPattern.length === gamePattern.length){
 
-      //5. Call nextSequence() after a 1000 millisecond delay.
+      //Call nextSequence() after a 1000 millisecond delay.
       setTimeout(function () {
         nextSequence();
       }, 1000);
 
     }
 
-  } else {
+  } 
+  //worng sequence pressed by user
+  else {
 
     var audio= new Audio("sounds/wrong.mp3");
     audio.play();
@@ -76,7 +91,7 @@ function checkAnswer(currentLevel){
   }
 }
 
-
+//After Game is over and click on restart, it initializes the variables with initial values
 function startOver(){
   level = 0;
   gamePattern = [];
